2f189b7861bfd44e5d9757ed8f9160cf727ee0f9
KaitoMajima <kaitomajima@outlook.com>
2022-04-13 19:47:38 -0300

    Add Menu Screen Template

---
+++ Scripts/ButtonColorSwitcher.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+
+namespace CyberBuggy.MenuScreenUtils
+{
+    public class ButtonColorSwitcher : MonoBehaviour, ISelectHandler, IDeselectHandler
+    {
+        [SerializeField] private Button _targetButton;
+        [SerializeField] private TMP_Text _buttonText;
+
+        [SerializeField] private Color _primaryColor = new Color(255, 255, 255, 255);
+        [SerializeField] private Color _secondaryColor;
+        public void OnSelect(BaseEventData eventData)
+        {
+            ApplyButtonColors(_secondaryColor, _primaryColor);
+        }
+
+        public void OnDeselect(BaseEventData eventData)
+        {
+            ApplyButtonColors(_primaryColor, _secondaryColor);
+        }
+  
+        private void ApplyButtonColors(Color firstColor, Color secondColor)
+        {
+            _targetButton.targetGraphic.color = firstColor;
+            _buttonText.color = secondColor;
+        }
+
+        
+    }
+}
